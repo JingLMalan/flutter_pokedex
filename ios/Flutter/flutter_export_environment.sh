596808d82f7c6f06e73ba6ee0d914b7f77395ed0
jing.l.malan <jing.l.malan@gmail.com>
2019-10-25 14:15:11 +0800

    add lock

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/mac/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/mac/Sources/flutter/pokedex"
-export "FLUTTER_TARGET=/Users/mac/Sources/flutter/pokedex/lib/main.dart"
+export "FLUTTER_ROOT=/Users/malanjing/Documents/flutter/sdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/malanjing/Desktop/projects/flutter/Pokedex/flutter_pokedex"
+export "FLUTTER_TARGET=/Users/malanjing/Desktop/projects/flutter/Pokedex/flutter_pokedex/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/mac/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/malanjing/Documents/flutter/sdk/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "TRACK_WIDGET_CREATION=true"
