dac37c62ceb0ffb4624c71fb51a252c5c250f8d4
jing.l.malan <jing.l.malan@gmail.com>
2019-10-25 14:24:47 +0800

    edit color

--- lib/data/categories.dart
+++ lib/data/categories.dart
@@ -2,10 +2,10 @@ import 'package:pokedex/configs/AppColors.dart';
 import 'package:pokedex/models/category.dart';
 
 const List<Category> categories = [
-  Category(name: "口袋王", color: AppColors.teal),
-  Category(name: "移动类", color: AppColors.red),
-  Category(name: "能力类", color: AppColors.blue),
-  Category(name: "物件", color: AppColors.yellow),
-  Category(name: "定位", color: AppColors.purple),
-  Category(name: "类型图标", color: AppColors.brown),
+  Category(name: "口袋王", color: AppColors.lightTeal),
+  Category(name: "移动类", color: AppColors.lightRed),
+  Category(name: "能力类", color: AppColors.lightBlue),
+  Category(name: "物件", color: AppColors.lightYellow),
+  Category(name: "定位", color: AppColors.lightPurple),
+  Category(name: "类型", color: AppColors.lightBrown),
 ];
